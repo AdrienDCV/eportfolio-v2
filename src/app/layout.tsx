660018bd0b258fc54ce3a2cd050d4d760a645c1e
Adrien DA COSTA VEIGA <adrien.dacostaveiga@jetdev.fr>
2025-09-13 23:26:04 +0200

    🌐➕🔧 : config internationalization next-intl

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,14 @@
 import './globals.css'
+import {NextIntlClientProvider} from "next-intl";
 
 export default function RootLayout({children,}: {
   children: React.ReactNode
 }) {
   return (
       <html lang="en">
-        <body>{children}</body>
+        <body>
+          <NextIntlClientProvider>{children}</NextIntlClientProvider>
+        </body>
       </html>
   )
 }
